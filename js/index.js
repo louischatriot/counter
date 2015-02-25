8be3be5e2c51be1f9dac0ed91fca76af36efcbf6
Louis Chatriot <louis.chatriot@gmail.com>
2015-02-25 20:07:43 +0100

    works

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var count = 0
-  , incrementPerSecond = 300
+  , incrementPerSecond = 30023
   , minDelay = 1900
   , maxDelay = 3700
   , latest = Date.now()
@@ -8,6 +8,33 @@ var count = 0
   , microDelay = 1
   ;
 
+// Quick and dirty padding function up to 3 digits
+function pad (n) {
+  if (n < 10) { return '00' + n; }
+  if (n < 100) { return '0' + n; }
+  return n.toString();
+}
+
+// Quick and dirty way to frmat as euros
+function formatAsEuros (amount) {
+  var res = '€'
+    , currentPart
+    ;
+
+  if (amount === 0) { return '0 ' + res; }
+
+  while (amount > 0) {
+    currentPart = amount - 1000 * Math.floor(amount / 1000);
+    if (amount === currentPart) {   // No padding if it's the last part
+      res = currentPart + ' ' + res;
+    } else {
+      res = pad(currentPart) + ' ' + res;
+    }
+    amount = (amount - currentPart) / 1000;
+  }
+
+  return res;
+}
 
 function getRandomDelay () {
   return Math.floor(minDelay + (maxDelay - minDelay) * Math.random());
@@ -28,7 +55,7 @@ function updateCounter () {
 function getCounterToValue (fromValue, toValue, beginTime) {
   var toDisplay = fromValue + (toValue - fromValue) * (Date.now() - beginTime) / updateTime;
   toDisplay = Math.min(toDisplay, toValue);
-  $counter.html(Math.floor(toDisplay));
+  $counter.html(formatAsEuros(Math.floor(toDisplay)));
   if (Date.now() - beginTime < updateTime) {
     setTimeout(function () { getCounterToValue(fromValue, toValue, beginTime); }, microDelay);
   }
@@ -39,3 +66,5 @@ function nowUpdating () {
   setTimeout(nowUpdating, getRandomDelay());
 }
 nowUpdating();
+
+
